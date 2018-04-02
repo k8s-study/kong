4a93057f9f23af63c29a92c76b1f0918b1d6234a
ashon <ashon8813@gmail.com>
2018-04-02 08:05:33 -0700

    Fix typo

--- tutorial/00_deploy_apigw.sh
+++ tutorial/00_deploy_apigw.sh
@@ -12,6 +12,6 @@ kubectl create -f k8s/kong-apigw/02_job__kong-migration.yml
 
 echo "-- deploy apigw services"
 sleep 5
-kubectl create -f k8s/kong-apigw/03_deploy__kong-apigw.yml
+kubectl create -f k8s/kong-apigw/03_deployment__kong-apigw.yml
 kubectl create -f k8s/kong-apigw/04_service__kong-apigw.yml
 kubectl create -f k8s/kong-apigw/05_ingress__kong-apigw.yml
