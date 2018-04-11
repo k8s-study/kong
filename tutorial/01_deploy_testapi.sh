f738a59c8a6df48eeabfa8f1eb8100ecb53945ff
ashon <ashon8813@gmail.com>
2018-04-11 22:46:34 +0900

    Add build step on 01_deploy_testapi.sh

--- tutorial/01_deploy_testapi.sh
+++ tutorial/01_deploy_testapi.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "-- build testapi on minikube"
+eval $(minikube docker-env)
+docker-compose build
+unset DOCKER_TLS_VERIFY DOCKER_HOST DOCKER_CERT_PATH DOCKER_API_VERSION
+
 echo "-- deploy testapi"
 kubectl create -f k8s/default
 
